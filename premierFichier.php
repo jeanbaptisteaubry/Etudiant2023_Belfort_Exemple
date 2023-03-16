e99f7e1eea9eb50bb077ea28ab51d4cc7fd86a1e
jeanbaptisteaubry <jean-baptiste.aubry@ac-besancon.fr>
2023-03-16 14:40:24 +0100

    commit pour montrer !

---
+++ premierFichier.php
@@ -0,0 +1,9 @@
+<?php
+//Pour afficher du texte => echo (python print)
+echo "Bonjour \n";
+echo "Quel est votre nom ? \n";
+//Pour saisir du texte => readline (python input)
+$nom = readline( );
+
+//Pour afficher une variable => echo et inscrustation de variable
+echo "Bonjour $nom \n";
